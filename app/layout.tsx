57aa259b16d2a3a7ec67f0380d31b9871d4154a4
Tarder-TCZ <uxorialauto63@gmail.com>
2026-05-06 15:52:29 +0530

    theme update

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,8 +83,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
         >
           {children}
         </ThemeProvider>
